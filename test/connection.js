bccc91f5ec793100aee1ed780c076d112b596694
maugares <mauro.garciaesteban@gmail.com>
2019-06-17 17:37:58 +0200

    Use ES6 promise library

--- test/connection.js
+++ test/connection.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection
-    .once('open', function () {
-        console.log('Connection has been made. now make fireworks...')
-    })
-    .on('error', function(error){
-        console.log('Connection error:', error)
-    });
+// Connect to the db before tests run
+before(function (done) {
+    // Connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
+
+    mongoose.connection
+        .once('open', function () {
+            console.log('Connection has been made. now make fireworks...')
+            done();
+        })
+        .on('error', function (error) {
+            console.log('Connection error:', error)
+        });
+});
