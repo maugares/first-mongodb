84211e121494e87bbd9a473c58ea86e65e41914d
maugares <mauro.garciaesteban@gmail.com>
2019-06-17 18:34:40 +0200

    Clean collection before running the test

--- test/connection.js
+++ test/connection.js
@@ -4,16 +4,24 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connect to the db before tests run
-before(function (done) {
+before((done) => {
     // Connect to mongodb
     mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
 
     mongoose.connection
-        .once('open', function () {
+        .once('open', () => {
             console.log('Connection has been made. now make fireworks...')
             done();
         })
-        .on('error', function (error) {
+        .on('error', (error) => {
             console.log('Connection error:', error)
         });
+});
+
+// Drop the character collection before each test
+beforeEach((done) => {
+    // Drop the collection
+    mongoose.connection.collections.mariochars.drop(() => {
+        done();
+    });
 });
