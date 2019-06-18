5cc0c479210f67d23e076737d6f1aefc195f5014
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 14:55:21 +0200

    Add method to delete a record

---
+++ test/deleting_test.js
@@ -0,0 +1,40 @@
+const mocha = require('mocha')
+const assert = require('assert');
+const MarioChar = require('../models/marioChar');
+
+// Describe our tests
+describe('Deleting records', () => {
+
+  let char;
+
+  beforeEach((done) => {
+    char = new MarioChar({
+      name: 'Mario'
+    });
+
+    char
+      .save()
+      .then(() => {
+        assert(!char.isNew);
+        done();
+      });
+  });
+
+  // Create tests
+  it('Deletes one record from the database',
+    (done) => {
+
+      MarioChar
+        .findOneAndDelete({ name: 'Mario' })
+        .then(() => {
+
+          MarioChar.findOne({ name: 'Mario' })
+            .then((result) => {
+              assert(result === null);
+              done();
+            })
+        });
+
+    });
+
+}); 
