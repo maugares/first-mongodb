13674e62f404adb913c32ea7385bb3f3907495d8
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 14:47:43 +0200

    Add empty lines in tests blocks

--- test/finding_test.js
+++ test/finding_test.js
@@ -23,21 +23,25 @@ describe('Finding records', () => {
   // Create tests
   it('Finds one record from the database',
     (done) => {
+      
       MarioChar
         .findOne({ name: 'Mario' })
         .then((result) => {
           assert(result.name === 'Mario');
           done();
         });
+
     });
 
   it('Finds one record by ID from the database',
     (done) => {
+
       MarioChar
         .findOne({ _id: char._id })
         .then((result) => {
           assert(result._id.toString() === char._id.toString());
           done();
         });
+
     });
 }); 
