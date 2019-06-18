6a185532f10ecd75bd74443aa30e6ab96f09f701
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 16:11:39 +0200

    Add test for adding an author with one book to the database

---
+++ test/nested_test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const mongoose = require('mongoose');
+const Author = require('../models/author')
+
+// Describe tests
+describe('Nesting records', () => {
+
+  // Create tests
+  it('Creates an author with sub-documents',
+    (done) => {
+
+      const pat = new Author({
+        name: 'Patrick Rothfuss',
+        books: [{ title: 'Name of the Wing', pages: 400 }]
+      });
+
+      pat
+        .save()
+        .then(() => {
+
+          Author
+            .findOne({ name: 'Patrick Rothfuss' })
+            .then((record) => {
+              assert(record.books.length === 1);
+              done();
+            });
+
+        });
+    });
+});
