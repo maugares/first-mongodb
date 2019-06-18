40c8e74d4dc55ca383f083423acd02006bbfd881
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 16:13:03 +0200

    Add a test for adding a book to an existing author

--- test/nested_test.js
+++ test/nested_test.js
@@ -33,4 +33,39 @@ describe('Nesting records', () => {
 
         });
     });
+
+  it('Adds a book to an existing author',
+    (done) => {
+
+      const pat = new Author({
+        name: 'Patrick Rothfuss',
+        books: [{ title: 'Name of the Wing', pages: 400 }]
+      });
+
+      pat
+        .save()
+        .then(() => {
+
+          Author
+            .findOne({ name: 'Patrick Rothfuss' })
+            .then((record) => {
+              // Add a book to the books collection
+              record.books.push(
+                { title: "Wise Man's Fear", pages: 500 }
+              );
+
+              record
+                .save()
+                .then(() => {
+
+                  Author
+                    .findOne({ name: 'Patrick Rothfuss' })
+                    .then((result) => {
+                      assert(result.books.length === 2);
+                      done();
+                    });
+                });
+            });
+        });
+    });
 });
