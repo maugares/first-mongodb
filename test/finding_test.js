6d695862c5087eb44b568d30dde7eecaba7a460b
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 14:41:25 +0200

    Add method to find a record by its ID

--- test/finding_test.js
+++ test/finding_test.js
@@ -5,8 +5,10 @@ const MarioChar = require('../models/marioChar');
 // Describe our tests
 describe('Finding records', () => {
 
+  let char;
+
   beforeEach((done) => {
-    const char = new MarioChar({
+    char = new MarioChar({
       name: 'Mario'
     });
 
@@ -25,6 +27,16 @@ describe('Finding records', () => {
         .findOne({ name: 'Mario' })
         .then((result) => {
           assert(result.name === 'Mario');
+          done();
+        });
+    });
+
+  it('Finds one record by ID from the database',
+    (done) => {
+      MarioChar
+        .findOne({ _id: char._id })
+        .then((result) => {
+          assert(result._id.toString() === char._id.toString());
           done();
         });
     });
